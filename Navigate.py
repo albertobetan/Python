02c093d54e5e7ccf501742a22c6ec57884079150
Albertobetan <albertobetan@outlook.com>
2019-10-28 16:43:09 -0700

    edit

--- Navigate.py
+++ Navigate.py
@@ -1,24 +1,22 @@
 #This code helps the user to navigate a file
-
 fileName = input("Enter the input file name: ")
 filehandle = open(fileName, 'r')
-line = filehandle.readline()
-while True:
-    count = 0
-    with open(fileName, 'r') as f:
-        for line in f:
-            count += 1
+fileList = []
+count = 0
+with open(fileName, 'r') as f:
+    for i in filehandle:
+        count += 1
+        fileList.append(i)
 
-    print("Total number of lines is:", count)
+print("Total number of lines is:", count)
+userLine = int(input("Enter a line number [0 to quit]: "))
 
-    userLine = int(input("Enter a line number [0 to quit]: "))
+while userLine != 0:
     if userLine is 0:
         print("You exited the program!!")
-        break
+    elif userLine > count:
+        print("ERROR!!!")
     else:
-        for i in userLine:
-            line = filehandle.readline()
-            i += 1
-        print(line)
+        print(fileList[userLine - 1])
 
 filehandle.close()
--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print('hello world')
+string = str(input("Enter a string of bits: "))
+
+firstChr = string[0]
+lastChr = string[-1]
+stringList = []
+secString = ""
+
+for ch in stringList:
+    secString += ch
+
+print(secString)
+a = secString[3:6]
+print(a)
